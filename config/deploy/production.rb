194bf4c022da16417e7cedb72765c42cd89f1056
Nabeel <nab2320@gmail.com>
2018-04-29 18:31:39 -0700

    heroku images fix

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,8 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-
+config.assets.compile = true
+config.assets.digest = true
 
 # role-based syntax
 # ==================
